ecc0685fd0f6d707adf4e6efcafcd0df9eee2be9
IzStriker <zjac104@live.rhul.ac.uk>
2022-07-11 19:08:33 +0100

    Feat: added register user functionality.

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,10 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
+import dotenv from "dotenv";
 
 // Local global css styles
-
 import "./assets/css/global.css";
 
+dotenv.config();
 createApp(App).use(store).use(router).mount("#app");
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,28 +1,35 @@
 <template>
   <div class="container">
-    <form class="form">
+    <form class="form" @submit="onSubmit">
       <h1>Register</h1>
       <div class="input-next-input">
         <div style="margin-right: 2px">
           <label>First Name</label>
-          <input type="text" />
+          <input v-model="formData.firstName" type="text" required />
         </div>
         <div>
-          <label>Second Name</label>
-          <input type="text" />
+          <label>Last Name</label>
+          <input v-model="formData.lastName" type="text" required />
         </div>
       </div>
       <div class="row">
         <label>Email</label>
-        <input type="text" />
+        <input v-model="formData.email" type="email" required />
       </div>
       <div class="row">
         <label>Password</label>
-        <input type="password" />
+        <input v-model="formData.password" type="password" required />
       </div>
       <div class="row">
         <label>Confirm Password</label>
-        <input type="password" />
+        <input v-model="formData.confirmPassword" type="password" required />
+      </div>
+      <div class="row error-message">
+        <ul>
+          <li v-for="error in errors" :key="error.description">
+            {{ error.description }}
+          </li>
+        </ul>
       </div>
       <div class="row">
         <input type="submit" value="Login" />
@@ -33,9 +40,40 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import { UserRegister, RegistrationError } from "@/models/user";
+import { registerUser } from "@/api";
 
 export default defineComponent({
   name: "Register",
+  data() {
+    return {
+      formData: {} as UserRegister,
+      errors: {} as RegistrationError[],
+    };
+  },
+  methods: {
+    async onSubmit(e: Event) {
+      e.preventDefault();
+      this.errors = [];
+      if (this.formData.password !== this.formData.confirmPassword) {
+        this.errors.push({
+          description: "Password and Confirm Password must match",
+        });
+        return;
+      }
+
+      try {
+        await registerUser(this.formData);
+        this.$router.push("/login");
+      } catch (error: any) {
+        if (error.response.data.message) {
+          this.errors.push({ description: error.response.data.message });
+        } else {
+          this.errors = [...this.errors, ...error.response.data.errors];
+        }
+      }
+    },
+  },
 });
 </script>
 
@@ -46,5 +84,9 @@ export default defineComponent({
 
 .input-next-input {
   display: flex;
+}
+
+.error-message {
+  color: red;
 }
 </style>
