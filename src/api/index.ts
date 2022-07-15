ce879b879dda2f7199595ce1c72e6d55288fe301
IzStriker <zjac104@live.rhul.ac.uk>
2022-07-15 16:34:14 +0100

    fix: elint config fix

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,11 +1,16 @@
-import axios, { AxiosResponse } from "axios";
-import { UserRegister, UserRegisterResponse, UserLogin, RegistrationError } from "@/models/user";
+import axios, { AxiosResponse } from 'axios';
+import {
+  UserRegister,
+  UserRegisterResponse,
+  UserLogin,
+  RegistrationError,
+} from '@/models/user';
 
 // Move to dotenv
-const BASE_URL = "/api";
+const BASE_URL = '/api';
 
 export const registerUser = (
-  user: UserRegister
+  user: UserRegister,
 ): Promise<AxiosResponse<UserRegisterResponse, RegistrationError>> => {
   return axios.post<UserRegisterResponse>(`${BASE_URL}/Auth/register/`, user);
 };
